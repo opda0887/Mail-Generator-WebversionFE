4f82fcacb26a76737902667de5dca33196ce2e78
Kiwi_Lin <opda0887@gmail.com>
2024-09-27 13:22:11 +0800

    prepare for the homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Home } from "./pages/home/Home";
 import { Header } from "./components/header/Header";
+import { Layout } from "./components/Layout";
 
 function App() {
-  return <div className="App"><Header /></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import "./Home.css";
+import background from "../../img/mainpage-bg.jpg";
+
+const Home = () => {
+  return (
+    <div className="homepage">
+      <h1>Welcome to the homepage</h1>
+    </div>
+  );
+};
+
+export { Home };
